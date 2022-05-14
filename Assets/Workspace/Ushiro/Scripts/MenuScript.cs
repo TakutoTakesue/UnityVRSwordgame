084e5c9c10bbacb4a00673cc04e44c8be4e93386
Ushiro10311 <sleeping10311@gmail.com>
2022-05-14 12:42:40 +0900

    ue

--- Assets/Workspace/Ushiro/Scripts/MenuScript.cs
+++ Assets/Workspace/Ushiro/Scripts/MenuScript.cs
@@ -4,62 +4,76 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using Math = System.Math;
+
+public enum MENU
+{
+    DEFAULT,
+    TITLE,
+    CLOSE
+}
 public class MenuScript : MonoBehaviour
 {
     [SerializeField]
-    Button[] DefaultButtons;
+    Button[] DefaultButtons;        //メニューを開いた時の画面のボタン
 
     [Space]
     [SerializeField]
-    Button[] TitleBackButtons;
+    Button[] TitleBackButtons;        //タイトルに戻るを押した時の確認画面のボタン
 
     [Space]
     [SerializeField]
-    Vector3[] OpenWindowSize;
+    Vector3[] OpeningWindowSize;        //メニューを開いた時のウィンドウのアニメーション
+
+    [SerializeField]
+    float OpeningWindowSpeed;        //メニューを開いた時のウィンドウのアニメーションのスピード
 
     [Space]
     [SerializeField]
-    Vector3[] CloseWindowSize;
+    Vector3[] ClosingWindowSize;        //メニューを閉じた時のウィンドウのアニメーション
 
     [SerializeField]
-    float WindowSizeChangeSpeed = 0;
+    float ClosingWindowSpeed;        //メニューを閉じた時のウィンドウのアニメーションのスピード
+    bool WindowScaleflg;               //今ウィンドウがアニメーションの途中かどうか
 
-    enum MENU
-    {
-        DEFAULT,
-        TITLE,
-        CLOSE
-    }
 
 
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        WindowScaleflg = false;
     }
 
 
-  public  void OpenMenu(Vector3 pos)
+    //メニューを開く
+    public void OpenMenu(Vector3 pos)
     {
-        transform.position = pos;
-
-        ButttonRecet();
-        for (int i = 0; i < DefaultButtons.Length; i++)
+        if (!WindowScaleflg)
         {
-            DefaultButtons[i].gameObject.SetActive(true);
+            transform.position = pos;
+
+            ButttonRecet();
+            for (int i = 0; i < DefaultButtons.Length; i++)
+            {
+                DefaultButtons[i].gameObject.SetActive(true);
+            }
+            StartCoroutine("OpeningMenu");
         }
-        StartCoroutine("OpeningMenu");
+    }
+
+    //メニューを閉じる
+    public void CloseMenu()
+    {
+
+        if (!WindowScaleflg)
+        {
+            ButttonRecet();
+            StartCoroutine("ClosingMenu");
+        }
     }
     void TitleBack()
     {
 
     }
-
+    //メニューのボタンを全部非アクティブ化する
     void ButttonRecet()
     {
         for (int i = 0; i < TitleBackButtons.Length; i++)
@@ -71,7 +85,7 @@ public class MenuScript : MonoBehaviour
             DefaultButtons[i].gameObject.SetActive(false);
         }
     }
-
+    //メニューの切り替え
     void ChangeMenu(MENU m)
     {
         ButttonRecet();
@@ -92,26 +106,68 @@ public class MenuScript : MonoBehaviour
             case MENU.CLOSE:
                 break;
         }
+
     }
+
+    //メニューを開くアニメーション
     IEnumerator OpeningMenu()
     {
-        int Count = 0;
-        Vector3 ThisScale =new Vector3(0.1f,0,0);
-        while (Count<OpenWindowSize.Length) {
-            if (Vector3.Distance(ThisScale,OpenWindowSize[Count])>0.01f)
-            {
+        WindowScaleflg = true;
+        int count = 1;
+        transform.localScale = OpeningWindowSize[0];
+        while (count < OpeningWindowSize.Length)
+        {
+            Vector3 thisScale = transform.localScale;
 
-                ThisScale.y = ThisScale.y - (ThisScale.y - OpenWindowSize[Count].y) * WindowSizeChangeSpeed;
-                ThisScale.x = ThisScale.x - (ThisScale.x - OpenWindowSize[Count].x) * WindowSizeChangeSpeed;
+            if (Vector3.Distance(thisScale, OpeningWindowSize[count]) > 0.1)
+            {
+                thisScale.x = thisScale.x + (OpeningWindowSize[count].x - thisScale.x) * OpeningWindowSpeed;
+                thisScale.y = thisScale.y + (OpeningWindowSize[count].y - thisScale.y) * OpeningWindowSpeed;
+                UnityEngine.Debug.Log(Vector3.Distance(thisScale, OpeningWindowSize[count]));
             }
             else
             {
-                ThisScale = OpenWindowSize[Count];
-                Count++;
+                UnityEngine.Debug.Log("Next");
+                thisScale = OpeningWindowSize[count];
+                count++;
             }
+            this.transform.localScale = thisScale;
 
-            this.transform.localScale = ThisScale;
             yield return new WaitForSeconds(0.01f);
-        } 
+        }
+        WindowScaleflg = false;
+    }
+
+
+
+    //メニューを閉じるアニメーション
+    IEnumerator ClosingMenu()
+    {
+        WindowScaleflg = true;
+        int count = 1;
+        transform.localScale = ClosingWindowSize[0];
+        while (count < ClosingWindowSize.Length)
+        {
+            Vector3 thisScale = transform.localScale;
+
+            if (Vector3.Distance(thisScale, ClosingWindowSize[count]) > 0.01)
+            {
+                thisScale.x = thisScale.x + (ClosingWindowSize[count].x - thisScale.x) * ClosingWindowSpeed;
+                thisScale.y = thisScale.y + (ClosingWindowSize[count].y - thisScale.y) * ClosingWindowSpeed;
+                UnityEngine.Debug.Log(Vector3.Distance(thisScale, ClosingWindowSize[count]));
+            }
+            else
+            {
+                UnityEngine.Debug.Log("Next");
+                thisScale = ClosingWindowSize[count];
+                count++;
+            }
+            this.transform.localScale = thisScale;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        ButttonRecet();
+        WindowScaleflg = false;
+        this.gameObject.SetActive(false);
     }
 }
